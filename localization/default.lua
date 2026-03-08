ae596805aaa055e99c4f98b3043822a0b78208cb
notmario <59957544+notmario@users.noreply.github.com>
2026-03-08 18:24:34 +0800

    first joker.  will go in collection order after this

---
+++ localization/default.lua
@@ -0,0 +1,15 @@
+return {
+    descriptions = {
+        Joker = {
+            j_mf_triangle = {
+                name = "Triangle",
+                text = {
+                    "Played cards each give",
+                    "{X:mult,C:white} X#1# {} Mult when scored",
+                    "if played hand is",
+                    "a {C:attention}Three of a Kind"
+                },
+            },
+        },
+    },
+}
--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
 FLUFF = SMODS.current_mod
 
 assert(SMODS.load_file("atlases.lua"))()
+
+-- we still use manual string loading for collection order.
+-- theres probably a better way but Whatever!
+local jokers = {
+    "triangle",
+}
+
+for _, path in ipairs(jokers) do
+    assert(SMODS.load_file("jokers/"..path..".lua"))()
+end
