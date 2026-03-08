160c04e7169ada8596f7a2184d5859bf1289e2ca
notmario <59957544+notmario@users.noreply.github.com>
2026-03-08 18:30:25 +0800

    i love commit farming

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 FLUFF = SMODS.current_mod
 
-assert(SMODS.load_file("atlases.lua"))()
+assert(SMODS.load_file("src/atlases.lua"))()
 
 -- we still use manual string loading for collection order.
 -- theres probably a better way but Whatever!
@@ -9,5 +9,5 @@ local jokers = {
 }
 
 for _, path in ipairs(jokers) do
-    assert(SMODS.load_file("jokers/"..path..".lua"))()
+    assert(SMODS.load_file("src/jokers/"..path..".lua"))()
 end
