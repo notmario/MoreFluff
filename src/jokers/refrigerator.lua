b3f56a956c5c846933036ccd3dfdee438c365377
notmario <59957544+notmario@users.noreply.github.com>
2026-04-02 23:32:00 +0800

    oops

--- src/jokers/refrigerator.lua
+++ src/jokers/refrigerator.lua
@@ -11,7 +11,7 @@ SMODS.Joker {
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	attributes = { "generation", "joker", },
+	attributes = { "joker", },
 	loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.odds }}
 	end,
