9562c18d87eff70d122da65af8bde861720a437e
notmario <59957544+notmario@users.noreply.github.com>
2026-04-04 23:03:21 +0800

    tag p

--- src/jokers/lettergem_p.lua
+++ src/jokers/lettergem_p.lua
@@ -14,20 +14,17 @@ SMODS.Joker({
 	cost = 3 * 2,
 	pos = { x = 6, y = 4 },
 	config = { extra = { per_ante = 0.9 } },
-	attributes = { },
+	attributes = { "xblindsize", },
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.per_ante, card.ability.extra.per_ante ^ G.GAME.round_resets.ante } }
 	end,
 	calculate = function(self, card, context)
-		if context.setting_blind and not card.getting_sliced then
-			local amount = G.GAME.blind.chips * (card.ability.extra.per_ante ^ G.GAME.round_resets.ante)
-			amount = amount - amount % (10 ^ math.floor(math.log10(amount) - 1))
-			G.GAME.blind.chips = amount
-			G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
-			return {
-				message = localize("k_reduced_ex"),
-				colour = G.C.PURPLE,
-			}
+		if context.setting_blind and not card.getting_sliced and G.GAME.round_resets.ante >= 0 then
+			local effects = {}
+			for i = 1, G.GAME.round_resets.ante do
+				effects[#effects + 1] = { xblindsize = card.ability.extra.per_ante }
+			end
+			return SMODS.merge_effects(effects)
 		end
 	end,
 })
