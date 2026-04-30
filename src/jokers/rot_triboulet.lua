0c8ea79a5d4c6b8889383b732f94cdd37f09d733
notmario <59957544+notmario@users.noreply.github.com>
2026-04-30 09:06:57 +0800

    oops

--- src/jokers/rot_triboulet.lua
+++ src/jokers/rot_triboulet.lua
@@ -20,7 +20,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	attributes = { "enhancements" },
+	attributes = { "xmult", "rank", "king", "queen", },
 	loc_vars = function(self, info_queue, center)
 	    return {
 	        vars = { center.ability.extra.xmult }
