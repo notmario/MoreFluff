414b4ac27fa996584e90114c8d8088ce66b8f2f8
notmario <59957544+notmario@users.noreply.github.com>
2026-05-06 08:36:17 +0800

    attributing it

--- src/jokers/rot_chicot.lua
+++ src/jokers/rot_chicot.lua
@@ -17,7 +17,7 @@ SMODS.Joker({
 	blueprint_compat = true,
 	eternal_compat = true,
 	perishable_compat = true,
-	attributes = {},
+	attributes = { "xblindsize" },
 	loc_vars = function(self, info_queue, center)
 	    return {
 	        vars = { center.ability.extra.xblindsize }
