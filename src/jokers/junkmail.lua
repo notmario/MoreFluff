ea65f1d67f10db0e6a99390ae90e3fa6b61da07e
notmario <59957544+notmario@users.noreply.github.com>
2026-05-20 02:56:05 +0800

    aaaagh

--- src/jokers/junkmail.lua
+++ src/jokers/junkmail.lua
@@ -10,7 +10,7 @@ SMODS.Joker({
 	cost = 6,
 	unlocked = true,
 	discovered = true,
-	blueprint_compat = true,
+	blueprint_compat = false,
 	eternal_compat = true,
 	perishable_compat = true,
 	attributes = { },
